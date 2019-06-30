fadd470e64a7f375a378d197e3939774533bef71
李比 <843298330@qq.com>
2019-06-30 17:05:19 +0800

    feat：完整依赖，搭建SpringBoot的脚手架

---
+++ src/main/java/com/libi/demo/JwtJpaSwaggerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.libi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtJpaSwaggerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtJpaSwaggerDemoApplication.class, args);
+    }
+
+}
