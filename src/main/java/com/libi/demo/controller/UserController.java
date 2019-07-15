d402fd76de2124aaef90abad1dc8c0d0c5802ceb
李比 <843298330@qq.com>
2019-07-15 19:11:03 +0800

    fix: 让我的格子变绿

--- src/main/java/com/libi/demo/controller/UserController.java
+++ src/main/java/com/libi/demo/controller/UserController.java
@@ -8,11 +8,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
-
-import javax.validation.constraints.NotNull;
 
 /**
  * @author :Libi
